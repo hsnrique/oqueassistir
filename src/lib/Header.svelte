d3844fe180a266a0d764bc50c01737b7948f028f
Henry Martins <68012319+henrymartinsb@users.noreply.github.com>
2023-06-19 11:12:39 -0300

    last cometas

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -11,7 +11,7 @@
 	</button>
 	<a
 		class="bg-white/20 hover:bg-white/50 shadow text-white hover:text-purple-600 font-medium rounded p-2 text-sm flex items-center"
-		href="https://github.com/henrymartinsb/oqueassistir"
+		href="https://github.com/henrymartinsb"
 	>
 		<span class="button_prefix__e8xDo">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
@@ -19,6 +19,6 @@
 			</svg>
 		</span
 		>
-		<span class="ml-2">Repositório</span>
+		<span class="ml-2">Dev</span>
 	</a>
 </div>
